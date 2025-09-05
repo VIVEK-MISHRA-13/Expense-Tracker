53e8e85f736d2a8a33e19379db1260528bfaa3db
Vivek Mishra <vm1343396@gmail.com>
2025-09-05 14:28:09 +0530

    login and signup created with two way binding in form and also done with profile pic upload

--- my-project/src/components/layouts/AuthLayout.jsx
+++ my-project/src/components/layouts/AuthLayout.jsx
@@ -1,24 +1,25 @@
 import React from 'react'
 import CARD_2 from "../../assets/images/card2.png"
+import { LuTrendingUpDown } from "react-icons/lu";
 
 const AuthLayout = ({children}) => {
   return (
     <div className='flex'>
         <div className='w-screen h-screen md:w-[60vw] px-12 pt-8 pb-12'>
-            <h2 className='text-lg font-medium text-black'>Expense Tracker</h2>
+            <h2 className='text-2xl font-semibold text-black'>Expense Tracker</h2>
             {children}
         </div>
 
         <div className='hidden md:block w-[40vw] h-screen bg-violet-50 bg-auth-bg-img bg-cover bg-no-repeat bg-center overflow-hidden p-8 relative'>
             <div className=' w-48 h-48 rounded-[40px] bg-purple-600 absolute -top-7 -left-5'></div>
-            <div className='w-48 h-5 rounded-[40px] border-[20px] border-fuchsia-600 absolute top-[30%] -right-100'></div>
+            <div className='w-48 h-52 rounded-[40px] border-[20px] border-fuchsia-600 absolute top-[30%] left-110'></div>
             <div className='w-48 h-48 rounded-[40px] bg-violet-500 absolute -bottom-7 -left-5 '></div>
 
             <div className='grid gird-cols-1 z-20'>
                 <StatsInfoCard
                 icon={<LuTrendingUpDown/>}
                 label="Track Your Income & Expenses"
-                value="430,000"
+                value="$430,000"
                 color="bg-primary"/>
             </div>
 
@@ -31,14 +32,14 @@ const AuthLayout = ({children}) => {
 export default AuthLayout;
 
 const StatsInfoCard = ({icon, label, value, color}) => {
-return <div className=''>
+return <div className='flex gap-6 bg-white p-4 rounded-xl shadow-md shadow-purple-400/10 border border-gray-200/50 z-10'>
   <div className={`w-12 h-12 flex items-center justify-center text-[26px] text-white ${color} rounded-full drop-shadow-xl `}>
       {icon}
   </div>
 
   <div>
-    <h6 className=''>{label}</h6>
-    <span className=''>{value}</span>
+    <h6 className='text-xs text-gray-500 mb-1'>{label}</h6>
+    <span className='text-[20px]'>{value}</span>
   </div>
 </div>
 }
